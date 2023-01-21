7204cf74779a1a43bbc32f2b68f542a3478d36dd
robertarotaritei <robert_aro@yahoo.com>
2023-01-21 19:29:21 +0100

    Updated api to use replicate

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,6 @@ FROM python:3.8-slim-buster
 WORKDIR /python-docker
 
 COPY requirements.txt requirements.txt
-RUN pip3 install numpy==1.24.1
-RUN pip3 install torch==1.13.1
-RUN pip3 install basicsr==1.4.1
 RUN pip3 install -r requirements.txt
 
 COPY . .
--- app.py
+++ app.py
@@ -1,41 +1,33 @@
 from flask import Flask, request
-from realesrgan import RealESRGANer
-from gfpgan import GFPGANer
-import cv2
-import base64
-import numpy as np
 from flask_cors import CORS
+import replicate
+import os
+from dotenv import load_dotenv
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
+os.environ['REPLICATE_API_TOKEN'] = os.getenv('REPLICATE_API_TOKEN')
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return 'No file present'
-    bg_upsampler = RealESRGANer(
-        scale=2,
-        model_path='models/RealESRGAN_x2plus.pth',
-        tile=400,
-        tile_pad=10,
-        pre_pad=0,
-        half=False)
-    face_enhancer = GFPGANer(
-        model_path='models/GFPGANv1.3.pth',
-        upscale=2,
-        arch='clean',
-        channel_multiplier=2,
-        bg_upsampler=bg_upsampler)
-    img_str = request.files['file'].read()
-    request.files['file'].close()
-
-    nparr = np.frombuffer(img_str, np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    _, _, img = face_enhancer.enhance(img_np, has_aligned=False, only_center_face=False, paste_back=True)
-    _, buffer = cv2.imencode('.jpg', img)
-    jpg_as_text = base64.b64encode(buffer)
-    return jpg_as_text
+    model = replicate.models.get("xinntao/realesrgan")
+    version = model.versions.get("1b976a4d456ed9e4d1a846597b7614e79eadad3032e9124fa63859db0fd59b56")
+    f = request.files['file']
+    f.save(f.filename)
+    inputs = {
+        'img': open(f.filename, 'rb'),
+        'version': "General - RealESRGANplus",
+        'scale': 2,
+        'face_enhance': False,
+        'tile': 16,
+    }
+    output = version.predict(**inputs)
+    print(output)
+    return output
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
